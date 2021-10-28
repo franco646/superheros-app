ec7ee51b98b13fd27bea626ede28baacc9c3ddec
franco646 <franco@>
2021-10-28 05:27:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ import bodyParser from "body-parser";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "frontend", "build")))
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
