a61ddfd2531544864ef6af331fd41b7a0eac36e7
Franco De Lorenzi <francodl646@gmail.com>
2024-06-04 22:26:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -43,6 +43,8 @@ app.use(returnError);
 
 const port = process.env.PORT || 8080;
 
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
